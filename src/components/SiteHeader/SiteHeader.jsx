6536ff6dacf0e97901c6506ba7e9f1cd988335a1
sbraende <sebastian.braende@gmail.com>
2025-03-25 10:57:20 +0100

    Title tweaked

--- src/components/SiteHeader/SiteHeader.jsx
+++ src/components/SiteHeader/SiteHeader.jsx
@@ -12,10 +12,10 @@ const SiteHeader = () => {
         Frontend Developer | Visual-Effects Veteran | Fairly nerdy | 🇳🇴
       </h2>
       <p className="site-header__paragraph">
-        My name is Sebastian Brænde, and I'm a frontend developer with a passion for building
-        engaging web experiences. I previously worked as Department lead of Lighting, Visual Effects
-        Supervisor, and Senior Visual Effects Artist for commercials, TV, and film, bringing stories
-        to life through visuals.
+        My name is Sebastian Brænde, and I'm a developer with a passion for building engaging web
+        experiences. I previously worked as Department Lead of Lighting, Visual Effects Supervisor,
+        and Senior Visual Effects Artist for commercials, TV, and film, bringing stories to life
+        through visuals.
       </p>
     </div>
   );
